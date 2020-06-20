27f933d6f0d41b1d45e071d8ae7a475bb66a2575
Abhijeet Dhamankar <adhamankar@gmail.com>
2020-06-20 17:58:47 +0530

    update prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,11 @@ import { version } from '../../package.json';
 export const environment = {
   production: true,
   appTitle: 'ProductPurpose',
+  VERSION: version, 
   templateFileLocation: 'https://raw.githubusercontent.com/adhamankar/productpurpose/master/templates',
-  VERSION: version
+  google: {
+    API_KEY: 'AIzaSyAkoFuREKxogSQ-uOuu0Oy07rRP7CcEj8Y',
+    CLIENT_ID: '304736345925-120qpq4eq3k67hptfmp76n12tnv8b7sl.apps.googleusercontent.com'
+  }
+
 };
